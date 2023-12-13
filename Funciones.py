6beea204d8d00b6f235861e9cbdb9adf20cb3829
Emmyrt <emmmyrtgame@gmail.com>
2023-12-13 13:17:23 -0600

    Avance 3

--- Funciones.py
+++ Funciones.py
@@ -1,7 +1,7 @@
 
 def di_hola(nombre, edad):
-    print("Hola " + nombre + ", tienes " + str(edad))
+    print("Hola " + nombre + ", tienes " + f"{edad:.2f}")
 
 print("=========")
-di_hola("Emmyrt", 16)
+di_hola("Emmyrt", 16.1616)
 print("=========")
